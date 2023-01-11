6c1f3b118065e805afe856ca0156045bdd1596a2
DNAisWHAT <DNAisWHAT@gmail.com>
2023-01-12 00:16:46 +0900

    Add 230111

---
+++ src/0111.js
@@ -0,0 +1,6 @@
+var fruitBox = 'grape'; // 선언문 + 대입연산자로 리터럴을 값으로 사용하다
+var grapeBox = 'grape'; // 선언문 + 대입연산자로 리터럴을 값으로 사용하다 2
+console.log(fruitBox === grapeBox); // true
+var luckyNumberInChina = 8; // 선언문 + 대입연산자로 리터럴을 값으로 사용하다 3
+var bigLuckyNumber = luckyNumberInChina > 7 ? luckyNumberInChina : 7; // 삼항 연산자를 사용하여 대입 연산자에 필요한 '값'을 만들어내다.
+console.log(bigLuckyNumber); // 8
